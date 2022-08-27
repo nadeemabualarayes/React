18ef0e5b2c8f51efc1dca2cd056d416eaad6d5e6
nadeemabualarayes <n.abualarayes@ngn.ps>
2022-08-27 11:18:05 +0300

    Update pointComp.jsx

--- src/Components/pointComp.jsx
+++ src/Components/pointComp.jsx
@@ -1,12 +1,20 @@
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../App";
 
 
 const PointComp = () => {
-    const { points } = useContext(AppContext);
+    const { points, setPoint } = useContext(AppContext);
+    const [addedPoint, setAddedPoint] = useState(0);
+    const addPoint = () => {
+        setPoint([...points, parseInt(addedPoint)]);
+    }
+
     return (
         <div>
+
+            <input type="number" value={addedPoint} onInput={(e) => setAddedPoint(e.target.value)}    placeholder="Add number" />
+            <button onClick={addPoint}>Add</button>
             <ul>
                 {points && points.length > 0 && points.map(item => <li> {item}</li>)}
             </ul>
