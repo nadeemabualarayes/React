42078767877ce36d3d6f9ae30b4619d15fd06ad9
nadeemabualarayes <n.abualarayes@ngn.ps>
2022-08-27 12:04:08 +0300

    add to do

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React, { createContext, useState } from "react";
 import "./App.css"
-import MyFirstForm from "./Components/MyFirstForm";
+import ToDoList from "./Components/ToDoList";
 import WrapperComp from "./Components/WrapperComp";
-
-//: Create Context .
 
 export const AppContext = createContext();
 
-
 const App = () => {
 
-  const [name, setName] = useState("Nadeem")
-  const [points, setPoints] = useState([1, 2, 3, 4, 5, 6, 7]);
-
+  const [name, setName] = useState("Ameer");
+  const [points, setPoints] = useState([1, 2, 3, 5, 6, 7])
   return <AppContext.Provider value={
-    { name, setName, points, setPoints }
-  }>
-    <WrapperComp />
-
+    {
+      name,
+      setName,
+      points,
+      setPoints
+    }
+  }  >
+    {/* <WrapperComp /> */}
+    <ToDoList />
   </AppContext.Provider>
-
-
-
-  /*   return <div>
-      <MyFirstForm />
-    </div> */
 }
 
 // Passed the originalcomponent 
---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,95 @@
+import { useReducer, useState } from "react";
+
+const initialToDos = [
+    {
+        id: "a1",
+        text: "Task 1",
+        status: "progressing",
+    },
+    {
+        id: "a2",
+        text: "Task 2",
+        status: "progressing",
+    },
+    {
+        id: "a3",
+        text: "Task 3",
+        status: "progressing",
+    },
+];
+
+const reducer = (state, action) => {
+    switch (action.type) {
+
+        case "add":
+            return [... state , action.item]
+
+        case "complete":
+            return state.map((item) => {
+                if (item.id === action.id) {
+                    return { ...item, status: "completed" };
+                } else {
+                    return item;
+                }
+            });
+        default:
+            return state;
+    }
+};
+
+const ToDoList = () => {
+    const [toDoList, dispatch] = useReducer(reducer, initialToDos);
+    //   text-decoration: line-through;
+
+    const [addedItem, setAddedItem] = useState({});
+
+    const handleChecked = (item) => {
+        dispatch({ type: "complete", id: item.id });
+    };
+
+    const handleAddTodo = (item) => {
+        dispatch({
+            type: "add", item: {
+                id: "a" + Math.floor(Math.random() * 1300),
+                text: addedItem,
+                status: "progressing"
+
+            }
+        });
+    };
+
+
+    return (
+        
+        <div>
+            <label>
+                <input
+                    type="text"
+                    value={addedItem}
+                    onInput={(e) => setAddedItem(e.target.value)}
+                />
+                <button onClick={() => handleAddTodo()}>
+                    Add
+                </button>
+            </label>
+
+
+            {toDoList && toDoList.map((item) => (
+                <div key={item.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={item.status === "completed"}
+                            onChange={() => handleChecked(item)}
+                        />
+                        {item.text}
+                    </label>
+                </div>
+            ))}
+
+         
+        </div>
+    );
+};
+
+export default ToDoList;
