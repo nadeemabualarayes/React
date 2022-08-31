6d2bb484ba4d57a124a8a859245fdd3e4866f474
nadeemabualarayes <n.abualarayes@ngn.ps>
2022-08-31 19:03:00 +0300

    use memo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import "./App.css"
+import "./App.css";
 import MyFirstForm from "./Components/MyFirstForm";
+import UseMemoExample from "./Components/UseMemoExample";
 
 const App = () => {
+  return (
+    <div>
+      {/*     <MyFirstForm /> */}
+      <UseMemoExample />
+    </div>
+  );
+};
 
-  return <div>
-    <MyFirstForm />
-  </div>
-}
-
-// Passed the originalcomponent 
+// Passed the originalcomponent
 // export default EnhancedComponent(App);
 export default App;
