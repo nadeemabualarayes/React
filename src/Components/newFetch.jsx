64c6a66805974c7d91f0fee93307c2ac5dce6f7e
nadeemabualarayes <n.abualarayes@ngn.ps>
2022-08-31 19:52:02 +0300

    new fetch

--- src/Components/newFetch.jsx
+++ src/Components/newFetch.jsx
@@ -1,8 +1,19 @@
+import { useState, useEffect, useRef } from "react";
 import useFetch from "./useFetch";
 
-const NewFetch = () => {
-  const todolist = useFetch("https://jsonplaceholder.typicode.com/todos");
-  return <>{todolist && todolist.map((item) => <div>{item.title}</div>)}</>;
+const NewFetch = (props) => {
+  const [limit, setLimit] = useState("10");
+  const [page, setPage] = useState("1");
+  const todoList = useFetch(
+    `https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}`
+  );
+
+  return (
+    <>
+      <input value={page} onInput={(e) => setPage(e.target.value)} />
+      {todoList && todoList.map((item) => <div>{item.title}</div>)}
+    </>
+  );
 };
 
 export default NewFetch;
--- src/Components/useFetch.js
+++ src/Components/useFetch.js
@@ -3,15 +3,15 @@ import { useEffect, useState } from "react";
 const useFetch = (url) => {
   const [fetchedArr, setFetchedArr] = useState([]);
   const fetchData = async () => {
-    const resp = await fetch(url);
-    const arrRecord = await resp.json();
-    setFetchedArr(arrRecord);
+    const response = await fetch(url);
+    const arrRecords = await response.json();
+    setFetchedArr(arrRecords);
   };
+
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [url]);
 
   return fetchedArr;
 };
-
 export default useFetch;
