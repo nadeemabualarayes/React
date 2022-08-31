8670963617e07a948d0ac2b2b8e704e40eb8ea17
nadeemabualarayes <n.abualarayes@ngn.ps>
2022-08-31 20:12:44 +0300

    Update newFetch.jsx

--- src/Components/newFetch.jsx
+++ src/Components/newFetch.jsx
@@ -1,16 +1,31 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import useFetch from "./useFetch";
 
 const NewFetch = (props) => {
   const [limit, setLimit] = useState("10");
-  const [page, setPage] = useState("1");
+  const [page, setPage] = useState(1);
   const todoList = useFetch(
     `https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}`
   );
 
+  const next = () => {
+    setPage(page + 1);
+  };
+  const back = () => {
+    setPage(page--);
+  };
+
+  //increase
+  const increase = () => {
+    setPage((page) => page + 1);
+  };
+
   return (
     <>
-      <input value={page} onInput={(e) => setPage(e.target.value)} />
+      <button onClick={next}>next</button>
+      <button onClick={back}>back</button>
+
+      {/* <input value={page} onInput={(e) => setPage(e.target.value)} /> */}
       {todoList && todoList.map((item) => <div>{item.title}</div>)}
     </>
   );
